7f41f6acb6fd211df95ef186ff21a47b31b5feab
DmitriySlepets <slepec_do@codeking.ru>
2018-11-26 09:45:53 +0300

    create AlertDialog - 2

--- app/src/main/java/ru/codeking/criminalintent/controllers/CrimeListActivity.java
+++ app/src/main/java/ru/codeking/criminalintent/controllers/CrimeListActivity.java
@@ -3,6 +3,7 @@ package ru.codeking.criminalintent.controllers;
 import android.support.v4.app.Fragment;
 
 import ru.codeking.criminalintent.fragments.CrimeListFragment;
+import ru.codeking.criminalintent.fragments.SingleFragmentActivity;
 
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
--- app/src/main/java/ru/codeking/criminalintent/fragments/SingleFragmentActivity.java
+++ app/src/main/java/ru/codeking/criminalintent/fragments/SingleFragmentActivity.java
@@ -1,4 +1,4 @@
-package ru.codeking.criminalintent.controllers;
+package ru.codeking.criminalintent.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
