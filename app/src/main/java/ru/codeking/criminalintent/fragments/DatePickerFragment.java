b290bc05004ec51ce080cefa29b830ea2f6f9ce4
DmitriySlepets <slepec_do@codeking.ru>
2018-12-01 21:18:08 +0300

    final DatePicker

--- app/src/main/java/ru/codeking/criminalintent/fragments/DatePickerFragment.java
+++ app/src/main/java/ru/codeking/criminalintent/fragments/DatePickerFragment.java
@@ -1,7 +1,10 @@
 package ru.codeking.criminalintent.fragments;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -10,10 +13,12 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import ru.codeking.criminalintent.R;
 
 public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE = "ru.codeking.criminalintent.date";
     private static final String ARG_DATE = "date";
     private DatePicker mDatePicker;
 
@@ -44,7 +49,27 @@ public class DatePickerFragment extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.data_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int year = mDatePicker.getYear();
+                        int month = mDatePicker.getMonth();
+                        int day = mDatePicker.getDayOfMonth();
+                        Date date = new GregorianCalendar(year,month,day).getTime();
+                        sendResult(Activity.RESULT_OK,date);
+                    }
+                })
                 .create();
+    }
+
+    private void sendResult(int resultCode, Date date){
+        if(getTargetFragment()==null){
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE,date);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(),resultCode,intent);
     }
 }
